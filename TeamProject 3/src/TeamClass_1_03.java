671a5397e5c8ac2c21fb5070a09f437c77284471
ryth <ryth@phantom>
2019-03-20 17:19:06 -0600

    Added a test file, which could be potentially used later on

---
+++ TeamProject 3/src/TeamClass_1_03.java
@@ -0,0 +1,14 @@
+/**
+ * This file is a test
+ * 	This could be used as the main .java file
+ * @author everyone
+ */
+public class TeamClass_1_03 {
+		// methods go here
+	
+	public static void main(String[] args) {
+		// TODO this
+
+	}
+
+}
